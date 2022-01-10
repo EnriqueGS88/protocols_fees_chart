1712e9f1b4f6f97eea4e598105021e98c0fef02b
EnriqueGS88 <enrique.gzs@gmail.com>
2022-01-10 18:53:38 +0100

    added scraping script

--- createScraperInputs.js
+++ createScraperInputs.js
@@ -1,7 +1,7 @@
-// List all dates between within a range period
-
+// This script will create 2 files in ./scraper_input
 const fs = require('fs-extra');
 
+// 1) Create listoOfDates.js 
 // Function to list all dates till Today in an Array
 let getDaysArray = function(start, end) {
     let arr=[];
@@ -15,11 +15,10 @@ let getDaysArray = function(start, end) {
 const today = new Date();
 const formatted_today = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()-1;
 
-// Call the function from date in the past to today's date
-// Below enter 'formatted_today' to set today's date
+// Below enter 'formatted_today' to set today's date or set a manual date below
 let daylist = getDaysArray(new Date("2021-12-01"), new Date("2021-12-31") );
 
-// Convert the format into one accepted by Dates scraper
+// Convert the date format into one accepted by cryptofees.info
 let daysRange = daylist.map( (v) => v.toISOString().slice(0,10));
 console.log("Number of Dates: ", daysRange.length);
 let days = daysRange.toString().replaceAll(',','","');
@@ -28,7 +27,7 @@ let days = daysRange.toString().replaceAll(',','","');
 let prefix = 'module.exports = ["';
 let listOfDates = prefix.concat(days, '"]');
 
-// Save output of dates into a JS file as an array
+// Function to save dates into a JS file as an array
 // where f = file path; and d = data to be written on file
 async function saveFile(f, d) {
     try {
@@ -40,17 +39,17 @@ async function saveFile(f, d) {
     }
 }
 
-let fileOfDates = './scraper_input/listOfDates.js';
-saveFile(fileOfDates, listOfDates);
+let pathOfDates = './scraper_input/listOfDates.js';
+saveFile(pathOfDates, listOfDates);
 
-
+// 2) Create listOfTimeouts.js
+// Create a timeout value for every position in the array listOfDates.js
+// Save that output in an Array file
 let stringOfDays = '["' + days + '"]';
 let arrayOfDays = JSON.parse(stringOfDays);
-console.log( arrayOfDays );
 
-// List all dates between within a range period
-// const listOfDates = require('./listOfDates');
-
+// Function to get two random numbers
+// Use these random numbers to calculate a timeout value fore very date
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -58,11 +57,10 @@ function getRandomInt(min, max) {
 }
 const timeoutsMap = arrayOfDays.map( x => (x.length*getRandomInt(120, 230) ) );
 
-// Convert format into a String that can be saved to a file
+// Convert the Array of timeouts into a String that can be saved to a file
 let timeoutsValues = 'module.exports = [' + timeoutsMap + ']';
 console.log(timeoutsValues);
 
-
-let fileOfTimeouts = './scraper_input/listOfTimeouts.js';
-saveFile(fileOfTimeouts, timeoutsValues);
+let pathOfTimeouts = './scraper_input/listOfTimeouts.js';
+saveFile(pathOfTimeouts, timeoutsValues);
 
